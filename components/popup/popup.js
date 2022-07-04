feeca03cf4ee5514df871d0224881319e31ab383
ilzira <murtazina.ilzi@yandex.ru>
2022-07-04 23:33:42 +0500

    select

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -21,18 +21,18 @@ $(() => {
     const $video = $(SELECTOR.VIDEO)
     const $btnPlay = $(SELECTOR.BTNPLAY)
 
-    $('.video__image-wrapper').on('click', function() {
+    $('.video__image-wrapper').on('click', function () {
         $($root).addClass(MODIFIER.OPENED)
         $($btnPlay).addClass(MODIFIER.PLAYSHOW)
     })
-    $('.popup__btn-close, .popup__overlay').click(function(){
+    $('.popup__btn-close, .popup__overlay').click(function () {
         $($root).removeClass(MODIFIER.OPENED)
         $($video).get(0).pause()
     })
-    $($btnPlay).on('click', function() {
+    $($btnPlay).on('click', function () {
         $($video).get(0).play()
         $($btnPlay).removeClass(MODIFIER.PLAYSHOW)
-        $($video).click(function() {
+        $($video).click(function () {
             this.paused ? this.play() : this.pause()
         })
     })
